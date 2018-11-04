46413c6d9575fa1ed0335ba4a8ef878fae1fa73e
MariAndrade <21972248+themaripool@users.noreply.github.com>
2018-11-04 13:03:23 -0200

    classe Jogo criada

---
+++ 1521730-1521816/src/Jogo.java
@@ -0,0 +1,11 @@
+
+public class Jogo {
+	
+	public Jogo() {
+		
+		Menu.b3.setEnabled(true);
+		Menu.b4.setEnabled(true);
+		
+	}
+
+}
--- 1521730-1521816/src/Main.java
+++ 1521730-1521816/src/Main.java
@@ -1,13 +1,4 @@
-
-
-import java.awt.Font;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-
-
-
 
 public class Main extends JFrame {
 
@@ -16,70 +7,24 @@ public class Main extends JFrame {
 	static JFrame frame = new JFrame("LUDO");
 	static int number;
 
-	
-	
-	//Botoes
-//	static JButton b1 = new JButton("Novo Jogo");
-//	static JButton b2 = new JButton("Carregar Jogo");
-//	static JButton b3 = new JButton("Salvar");
-//	static JButton b4 = new JButton("Lancar Dado");
-	
-//	//Labels
-//	static JLabel turno = new JLabel("À Jogar: ");
-//	public static Font turno25 = new Font("Courier New", Font.BOLD, 25);
-	
 	public static void main(String[] args)
 	 {
-//		ConfiguraSaveGame();
-//		ConfiguraLoadGame();
-//		ConfiguraLancaDado();
-//		ConfiguraNewGame();
-//		ConfiguraLabel();
+
 		ConfiguraFrame();
-//		
-//		frame.add(b1);
-//		frame.add(b2);
-//		frame.add(b3);
-//		frame.add(b4);
-//		frame.add(turno);
-	
+
 		Tabuleiro tab = new Tabuleiro();
 		
-		
 		frame.add(tab);
-		
 		frame.setVisible(true);	
 		frame.repaint();
 	 }
 	
-//	static void ConfiguraNewGame() {
-//		b1.setBounds(620,50,150,50);
-//	}
-//	static void ConfiguraLoadGame() {
-//		 b2.setBounds(620,110,150,50);
-//	}
-//	static void ConfiguraSaveGame() {
-//		b3.setBounds(620,170,150,50); 
-//		b3.setEnabled(false);
-//	}
-//
-//	static void ConfiguraLabel() {
-//		turno.setFont(turno25);
-//		turno.setBounds(630, 235, 180, 30);
-//	}
-//
-//	static void ConfiguraLancaDado() {
-//		b4.setBounds(620,390,150,50);
-//		b4.addActionListener(new TratadorBotao(b4));
-//	
-//	}
-
 	static void ConfiguraFrame() {
-		frame.setSize(800, 635);
+		
+		frame.setSize(800, 620); //mac
+		//frame.setSize(800, 640); //windows
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			
 	}
-
-
 }
